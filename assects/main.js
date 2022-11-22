a2bda771c2c8ea3197c1583b644ac18eaff7c8d7
leusio jose filimone <leusiojosefilimone@gmail.com>
2022-11-22 12:27:52 -0500

    slide menu concluido

---
+++ assects/main.js
@@ -0,0 +1,7 @@
+function openMenu(){
+    let nav = document.querySelector("nav")
+    nav.classList.toggle("open")
+
+    let menu = document.querySelector(".mobile-menu")
+    menu.classList.toggle("opened")
+}
